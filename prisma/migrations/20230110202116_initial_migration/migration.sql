7db13572932ed83429e6c8aa16f8ecb90a2bb099
JR <josequintana2804@gmail.com>
2023-01-10 14:40:29 -0500

    Update prisma schema

---
+++ prisma/migrations/20230110202116_initial_migration/migration.sql
@@ -0,0 +1,48 @@
+-- CreateTable
+CREATE TABLE "Account" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "addressId" TEXT NOT NULL,
+    CONSTRAINT "Account_addressId_fkey" FOREIGN KEY ("addressId") REFERENCES "Address" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Address" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "street" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "state" TEXT NOT NULL,
+    "zip" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Order" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "subtotal" REAL NOT NULL,
+    "taxes" REAL NOT NULL,
+    "total" REAL NOT NULL,
+    "accountId" TEXT,
+    CONSTRAINT "Order_accountId_fkey" FOREIGN KEY ("accountId") REFERENCES "Account" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "OrderItem" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "quantity" INTEGER NOT NULL,
+    "price" REAL NOT NULL,
+    "orderId" TEXT,
+    "productId" TEXT NOT NULL,
+    CONSTRAINT "OrderItem_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "Order" ("id") ON DELETE SET NULL ON UPDATE CASCADE,
+    CONSTRAINT "OrderItem_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "price" REAL NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Account_addressId_key" ON "Account"("addressId");
