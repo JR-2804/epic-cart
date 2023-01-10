ce46ca8c5ffc29ef79e98d2bcd17ccb29e33be7a
JR <josequintana2804@gmail.com>
2023-01-10 14:01:20 -0500

    Implement base pages

---
+++ src/pages/order/create.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const CreateOrderPage: NextPage = () => (
+  <>
+    <Head>
+      <title>Create Order</title>
+      <meta name="description" content="Salesforce interview project" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <div className="grid">Create Order</div>
+  </>
+);
+
+export default CreateOrderPage;
---
+++ src/pages/order/success.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const SuccessPage: NextPage = () => (
+  <>
+    <Head>
+      <title>Success</title>
+      <meta name="description" content="Salesforce interview project" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <div className="grid">Success</div>
+  </>
+);
+
+export default SuccessPage;
