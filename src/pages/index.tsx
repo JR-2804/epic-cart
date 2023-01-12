f5bd9537ff227cc1a0053af56c705a6759ae77fa
JR <josequintana2804@gmail.com>
2023-01-11 22:51:59 -0500

    Improve index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,47 +1,49 @@
-import { type NextPage } from "next";
-import Head from "next/head";
-import { Combobox } from "@headlessui/react";
 import { useState } from "react";
-import type { GetStaticProps } from "next";
-import { prisma } from "../server/db";
+import type { GetStaticProps, NextPage } from "next";
+import Head from "next/head";
 import Link from "next/link";
+import { Combobox } from "@headlessui/react";
+import { prisma } from "../server/db";
 
 const HomePage: NextPage<{ accounts: string[] }> = ({ accounts }) => {
-  const [selectedPerson, setSelectedPerson] = useState();
+  const [selectedAccount, setSelectedAccount] = useState();
   const [query, setQuery] = useState("");
 
-  const filteredAccounts =
-    query === ""
-      ? accounts
-      : accounts.filter((person) => {
-          return person.toLowerCase().includes(query.toLowerCase());
-        });
+  const matchesQuery = (query: string) => (account: string) =>
+    account.toLowerCase().includes(query.toLowerCase());
+
+  const filteredAccounts = accounts.filter(matchesQuery(query));
 
   return (
     <>
       <Head>
-        <title>Salesforce cart</title>
+        <title>Salesforce Cart</title>
         <meta name="description" content="Salesforce interview project" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="grid h-screen place-content-center gap-4">
-        <p>Select account ({selectedPerson})</p>
-        <Combobox value={selectedPerson} onChange={setSelectedPerson}>
+        <p>Select account ({selectedAccount})</p>
+        <Combobox value={selectedAccount} onChange={setSelectedAccount}>
           <Combobox.Input
             onChange={(event) => setQuery(event.target.value)}
             placeholder="Nombre de la cuenta..."
             className="rounded-lg bg-red-300 p-2 placeholder:text-zinc-500"
           />
           <Combobox.Options className="bg-red-600">
-            {filteredAccounts.map((person) => (
-              <Combobox.Option key={person} value={person}>
-                {person}
+            {filteredAccounts.map((account) => (
+              <Combobox.Option key={account} value={account}>
+                {account}
               </Combobox.Option>
             ))}
           </Combobox.Options>
         </Combobox>
         <Link
-          href="/order/create"
+          href={{
+            pathname: "/order/create",
+            query: {
+              account: selectedAccount,
+            },
+          }}
           className="bg-slate-500 text-center hover:bg-slate-600"
         >
           Create Order
@@ -55,6 +57,8 @@ export default HomePage;
 
 export const getStaticProps: GetStaticProps = async () => ({
   props: {
-    accounts: await prisma.account.findMany(),
+    accounts: (await prisma.account.findMany()).map(
+      ({ name }: { name: string }) => name
+    ),
   },
 });
