55ca901c8fd74c62411b50b043725e94cf0cbf5d
JR <josequintana2804@gmail.com>
2023-01-12 02:44:41 -0500

    Transform example router to order

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,14 +1,8 @@
 import { createTRPCRouter } from "./trpc";
-import { exampleRouter } from "./routers/example";
+import { orderRouter } from "./routers/order";
 
-/**
- * This is the primary router for your server.
- *
- * All routers added in /api/routers should be manually added here
- */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
+  order: orderRouter,
 });
 
-// export type definition of API
 export type AppRouter = typeof appRouter;
