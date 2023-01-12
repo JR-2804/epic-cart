92866b00d8213c169bf48442e1caa69f30377a2d
JR <josequintana2804@gmail.com>
2023-01-11 22:52:16 -0500

    Improve create page

--- src/pages/order/create.tsx
+++ src/pages/order/create.tsx
@@ -1,15 +1,44 @@
-import type { NextPage } from "next";
+import type { Product } from "@prisma/client";
+import type { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
+import { useRouter } from "next/router";
+import { prisma } from "../../server/db";
 
-const CreateOrderPage: NextPage = () => (
-  <>
-    <Head>
-      <title>Create Order</title>
-      <meta name="description" content="Salesforce interview project" />
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-    <div className="grid">Create Order</div>
-  </>
-);
+const CreateOrderPage: NextPage<{ products: Product[] }> = ({ products }) => {
+  const router = useRouter();
+  const { account } = router.query;
+
+  return (
+    <>
+      <Head>
+        <title>Create Order</title>
+        <meta name="description" content="Salesforce interview project" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="container grid h-screen grid-cols-2 gap-6 py-8">
+        <div className="bg-red-500">
+          <input type="text" placeholder="Filter products" />
+          {products.length === 0 && <h6>No existen productos</h6>}
+          {products.length > 0 &&
+            products.map((product) => (
+              <div key={product.id}>
+                <p>{product.name}</p>
+                <button type="button">Add</button>
+              </div>
+            ))}
+        </div>
+        <div className="bg-blue-500">
+          <h1>New Order: {account}</h1>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default CreateOrderPage;
+
+export const getStaticProps: GetStaticProps = async () => ({
+  props: {
+    products: await prisma.product.findMany(),
+  },
+});
