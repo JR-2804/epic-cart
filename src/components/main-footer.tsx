db208b035062516f28dbda67d863d40994824b21
JR <josequintana2804@gmail.com>
2023-01-13 19:19:33 -0500

    Implement footer design

--- src/components/main-footer.tsx
+++ src/components/main-footer.tsx
@@ -1,6 +1,13 @@
 const MainFooter = () => (
-  <footer className="grid max-h-[200px] overflow-auto bg-pink-400 p-4">
-    Footer
+  <footer className="rounded-lg bg-white p-4 shadow dark:bg-gray-900 md:px-6 md:py-8">
+    <hr className="my-6 border-gray-200 dark:border-gray-700 sm:mx-auto lg:my-8" />
+    <span className="block text-sm text-gray-500 dark:text-gray-400 sm:text-center">
+      <span>© 2023 </span>
+      <a href="https://flowbite.com/" className="hover:underline">
+        ZenCode™.
+      </a>
+      <span> All Rights Reserved.</span>
+    </span>
   </footer>
 );
 
