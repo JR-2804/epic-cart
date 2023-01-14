884f72d2caa557ca687b985d4202b33543ac79a7
JR <josequintana2804@gmail.com>
2023-01-14 00:35:38 -0500

    make submit order button disable if cart is empty

--- src/components/order-cart-submit.tsx
+++ src/components/order-cart-submit.tsx
@@ -41,7 +41,7 @@ const OrderCartSubmit = () => {
       <button
         type="button"
         className="grid h-10 w-44 grid-flow-col place-content-center items-center rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 disabled:cursor-not-allowed dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        disabled={submitOrderMutation.isLoading}
+        disabled={submitOrderMutation.isLoading || !cart.total}
         onClick={() => void submitOrder()}
       >
         {submitOrderMutation.isLoading ? (
