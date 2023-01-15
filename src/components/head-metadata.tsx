3d088ce3cec56eef536b03b9bd1e190fbff71c3a
JR <josequintana2804@gmail.com>
2023-01-15 07:13:46 -0500

    Change app name

--- src/components/head-metadata.tsx
+++ src/components/head-metadata.tsx
@@ -3,7 +3,7 @@ import Head from "next/head";
 const MetadataHead = ({ title }: { title: string }) => (
   <Head>
     <title>{title}</title>
-    <meta name="description" content="Salesforce interview project" />
+    <meta name="description" content="Best shop cart ever" />
     <link rel="icon" href="/favicon.ico" />
   </Head>
 );
