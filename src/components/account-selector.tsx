3d21e6cf1eb199845979a49fa90d7e97215f4f0f
JR <josequintana2804@gmail.com>
2023-01-16 05:00:11 -0500

    Add autoFocus to account selector

--- src/components/account-selector.tsx
+++ src/components/account-selector.tsx
@@ -37,6 +37,7 @@ const AccountSelector = ({ accounts }: { accounts: string[] }) => {
         onChange={updateQuery}
         className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-4 pl-10 text-sm text-gray-900 outline-none focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
         placeholder="Search accounts..."
+        autoFocus={true}
       />
       {query && query === selectedAccount && <ValidIcon />}
       {query && query !== selectedAccount && (
